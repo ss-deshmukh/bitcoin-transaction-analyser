0e41da58aeafbdf9f61d6291e60e8296eda7d9f3
Simonig <simon.ig13@gmail.com>
2024-05-29 09:54:03 +0200

    Refactor create_db

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from app.models.bitcoin_transaction import BitcoinTransaction
+from app.models.tracked_transaction import TrackedTransaction
+from app.models.known_addresses import KnownAddress
+from app.models.bitcoin_address import BitcoinAddress
+
+__all__ = [
+    "BitcoinTransaction",
+    "TrackedTransaction",
+    "KnownAddress",
+    "BitcoinAddress",
+]
--- create_db.py
+++ create_db.py
@@ -1,21 +1,11 @@
 # create_db.py
 
-from sqlalchemy import create_engine, Column, Integer, String, Date, Sequence
-from sqlalchemy.orm import declarative_base
+from app import Base
+from app.models import (BitcoinAddress, BitcoinTransaction, KnownAddress,
+                        TrackedTransaction)
 from config import DATABASE_URL
-
-Base = declarative_base()
-
-class BitcoinAddress(Base):
-    __tablename__ = 'btc_addresses'
-
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    address = Column(String(255), unique=True, nullable=False)
-    times_in_mixer = Column(Integer, nullable=False)
-    criminal_activities = Column(Integer, nullable=False)
-    high_volume_transactions = Column(Integer, nullable=False)
-    last_active_date = Column(Date, nullable=False)
-
+from sqlalchemy import Column, Date, Integer, Sequence, String, create_engine
+from sqlalchemy.orm import declarative_base
 
 if not DATABASE_URL:
     raise ValueError("No DATABASE_URL found. Please check your configuration.")
